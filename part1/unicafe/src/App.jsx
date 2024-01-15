4e38b19b425513f351923bd651cb01fbb4453deb
Matias Tardones <mtardones99@gmail.com>
2024-01-15 13:22:27 -0300

    arreglos de directorios

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -43,22 +43,10 @@ const StatisticsLine = ({ text, value }) => {
 };
 const App = () => {
   // guarda los clics de cada botón en su propio estado
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.',
-  ];
-
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const [selected, setSelected] = useState(0);
-  const [vote, setVote] = useState(anecdotes.map(() => 0));
+
 
   const handleClickGood = () => {
     // console.log('Prueba de click GOOD');
@@ -101,21 +89,6 @@ const App = () => {
     return promedioP * 100;
   }
   // console.log('El promedio es:', positive(good, all));
-  const handleClickAnecdote = () => {
-    const randomNumber = Math.floor(Math.random() * anecdotes.length);
-    setSelected(randomNumber);
-  };
-  const handleClickVote = () => {
-    const copyVote = [...vote];
-    copyVote[selected] += 1;
-    setVote(copyVote);
-  };
-
-  const mostVote = vote.reduce((maxIndex, currentVotes, currentIndex) => {
-    return currentVotes > vote[maxIndex] ? currentIndex : maxIndex;
-  }, 0);
-
-  const anecdotaConMasVotos = anecdotes[mostVote];
 
   return (
     <>
@@ -134,13 +107,6 @@ const App = () => {
         funcionAverage={average(good, bad, all)}
         funcionPositive={positive(good, all)}
       />
-      <div>{anecdotes[selected]}</div>
-      <p>has {vote[selected]}</p>
-      <button onClick={handleClickVote}> vote</button>
-      <button onClick={handleClickAnecdote}>next anecdotes</button>
-      <h1>Anecdote with most votes</h1>
-      <p>{anecdotaConMasVotos}</p>
-      <p>has {vote[mostVote]}</p>
     </>
   );
 };
