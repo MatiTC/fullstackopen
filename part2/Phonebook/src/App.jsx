9e0654094c65613dc4d6a9e6bd17a0ad88b17d26
Matias Tardones <mtardones99@gmail.com>
2024-01-26 20:02:57 -0300

    Exercise 2.12 axios persons

--- part2/Phonebook/src/App.jsx
+++ part2/Phonebook/src/App.jsx
@@ -1,26 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PersonForm from './componets/PersonForm';
 import Filter from './componets/Filter';
 import Persons from './componets/Persons';
 import OneUser from './componets/OneUser';
+import axios from 'axios';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [search, setSearch] = useState('');
+  console.log(persons);
+  useEffect(() => {
+    console.log('Ejecutando el useEffect');
+    //Realiza la solicitud GET al servidor JSON simulado con json-server
+    axios
+      .get('http://localhost:3001/persons')
+      .then((response) => {
+        console.log('promesa existosa. Datos de data:', response.data);
+        setPersons(response.data);
+      })
+      .catch((error) => {
+        console.error('Error al obtener los datos', error);
+      });
+  }, []); // Este paramaetro [] asegura que el useEffect se ejecute solo una vez
+  console.log(persons);
   return (
     <div>
       <h2>Phonebook</h2>
       <Filter search={search} setSearch={setSearch} />
       <PersonForm persons={persons} setPersons={setPersons} />
       <h2>Numbers</h2>
-      <Persons persons={persons} search={search}/>
+      <Persons persons={persons} search={search} />
       <h2>Detalles del Usuario </h2>
-      <OneUser user={persons[0]}/>
+      <OneUser user={persons[0]} />
     </div>
   );
 };
--- part2/Phonebook/src/componets/OneUser.jsx
+++ part2/Phonebook/src/componets/OneUser.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 const OneUser = ({user}) => {
     console.log(`Usuario por peticion de app:`, user)
+    if (!user) {
+      return null; // O un mensaje indicando que no hay datos
+    }
   return (
     <div>
         <ul>
