40927c3216e016438cc403acead2c2cfc9cbc606
Matias Tardones <mtardones99@gmail.com>
2024-03-11 20:58:54 -0300

    exercices 4.5

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -5,7 +5,30 @@ const dummy = (blogs) => {
 const totalLikes = (blogs) => {
   return blogs.reduce((total, blog) => total + blog.likes, 0);
 };
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null;
+  }
+  let maxLikes = 0;
+  let favorite = null;
+
+  blogs.forEach((blog) => {
+    if (blog.likes > maxLikes) {
+      maxLikes = blog.likes;
+      favorite = blog;
+    }
+  });
+
+  if (favorite) {
+    delete favorite._id;
+    delete favorite.__v;
+    delete favorite.url;
+  }
+  return favorite;
+};
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
