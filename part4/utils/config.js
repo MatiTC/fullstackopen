dd87b7fcbd60b2805b4266610f35ea65178ca895
Matias Tardones <mtardones99@gmail.com>
2024-03-15 17:25:37 -0300

    exercices 4.8

--- part4/utils/config.js
+++ part4/utils/config.js
@@ -1,9 +1,11 @@
+/* eslint-disable no-undef */
 require('dotenv').config();
-
-// eslint-disable-next-line no-undef
 const PORT = process.env.PORT; // || 3001
-// eslint-disable-next-line no-undef
-const MONGODB_URL = process.env.MONGODB_URL;
+
+const MONGODB_URL =
+  process.env.NODE_ENV === 'test'
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGODB_URL;
 
 module.exports = {
   MONGODB_URL,
