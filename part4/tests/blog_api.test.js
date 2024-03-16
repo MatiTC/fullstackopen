8d700ef67babe5a941edc77f124a48988e9bb051
Matias Tardones <mtardones99@gmail.com>
2024-03-15 21:13:08 -0300

    exercice 4.9

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -3,12 +3,36 @@ const supertest = require('supertest');
 const app = require('../app');
 
 const api = supertest(app);
+describe('testBlogs', () => {
+  test('las notas se devuelven como json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+  });
 
-test('retorna lista de de blogs correcta', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
+  test('hay 4 blogs', async () => {
+    const response = await api.get('/api/blogs');
+
+    expect(response.body).toHaveLength(4);
+  });
+  //exercise 4.8
+  test('4 blogs y formato json', async () => {
+    const response = await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+
+    expect(response.body).toHaveLength(4);
+  });
+
+  test('las publicaciones de blog tienen un identificador único llamado _id', async () => {
+    const response = await api.get('/api/blogs');
+
+    response.body.forEach((blog) => {
+      expect(blog.id).toBeDefined();
+    });
+  });
 });
 
 afterAll(() => {
