536253f9471971f527892f9fc749f9cdd8720a11
Matias Tardones <mtardones99@gmail.com>
2024-03-30 19:31:00 -0300

    arreglos

--- part5/src/components/Blog.jsx
+++ part5/src/components/Blog.jsx
@@ -1,19 +1,26 @@
 import React, { useState } from 'react';
-import '../components/style/blog.css';
-import Taggable from './Togglable';
+import '../components/style/blog.css'
 
 const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const toggleDetails = () => {
+    setShowDetails(!showDetails);
+  };
 
   return (
     <div className="blogStyle">
-      <div>{blog.title} </div>
-      <Taggable  buttonLabel="Detalles">
+      <div>{blog.title}</div>
+      {showDetails && (
         <div>
           <p>Autor: {blog.author}</p>
-          <p>Url: {blog.url}</p>
-          <p>Likes: {blog.likes}</p>
+          <p>Url: {blog.content}</p>
+          {/* Agrega más campos aquí según los atributos de tu blog */}
         </div>
-      </Taggable>
+      )}
+      <button onClick={toggleDetails}>
+        {showDetails ? 'Hide Details' : 'Show Details'}
+      </button>
     </div>
   );
 };
