fd923cc11471b714b1253d71be98e55fe742b9ab
Matias Tardones <mtardones99@gmail.com>
2024-04-15 16:54:19 -0400

    exercices 7.1

---
+++ part7/routed-anecdotes/.eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    indent: ['error', 2], // Indentation de 2 espacios
+    quotes: ['error', 'single'], // Utilizar comillas simples para cadenas de texto
+    semi: ['error', 'always'], // Requiere que los puntos y coma se coloquen al final de cada declaración
+    eqeqeq: 'error', // Requiere el uso estricto del operador de igualdad (===) y desalienta el uso de la comparación débil (==)
+    'no-trailing-spaces': 'error', // No debe haber espacios en blanco al final de las líneas de código
+    'object-curly-spacing': ['error', 'always'], // Espacios dentro de llaves en objetos
+    'array-bracket-spacing': ['error', 'never'], // Espacios dentro de los arrays
+    'no-multiple-empty-lines': ['error', { max: 1 }], // Controlar el número máximo de líneas en blanco consecutivas permitidas en el código
+    'no-console': 0, // Permitir el uso de la consola
+    'react/react-in-jsx-scope': 'off', // No es obligatorio importar React en archivos JSX
+    'react/prop-types': 0, // No es obligatorio definir propTypes para los componentes de React
+    'no-unused-vars': 0, // Se permiten variables no utilizadas
+    'jest/no-identical-title': 'off',
+    "react/no-unescaped-entities": [true, { "forbid": ["<", ">", "&", "\"", "'"] }]
+    //"linebreak-style": ["error", "unix"],
+  },
+};
---
+++ part7/routed-anecdotes/src/App.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import Menu from './component/Menu';
+import About from './component/About';
+import Footer from './component/Footer';
+import AnecdoteList from './component/AnecdoteList';
+import CreateNew from './component/CreateAnecdote';
+
+const App = () => {
+  const [anecdotes, setAnecdotes] = useState([
+    {
+      content: 'If it hurts, do it more often',
+      author: 'Jez Humble',
+      info: 'https://martinfowler.com/bliki/FrequencyReducesDifficulty.html',
+      votes: 0,
+      id: 1,
+    },
+    {
+      content: 'Premature optimization is the root of all evil',
+      author: 'Donald Knuth',
+      info: 'http://wiki.c2.com/?PrematureOptimization',
+      votes: 0,
+      id: 2,
+    },
+  ]);
+
+  const [notification, setNotification] = useState('');
+
+  const addNew = (anecdote) => {
+    anecdote.id = Math.round(Math.random() * 10000);
+    setAnecdotes(anecdotes.concat(anecdote));
+  };
+
+  const anecdoteById = (id) => anecdotes.find((a) => a.id === id);
+
+  const vote = (id) => {
+    const anecdote = anecdoteById(id);
+
+    const voted = {
+      ...anecdote,
+      votes: anecdote.votes + 1,
+    };
+
+    setAnecdotes(anecdotes.map((a) => (a.id === id ? voted : a)));
+  };
+
+  return (
+    <>
+      <h1>Software anecdotes</h1>
+      <Menu />
+      <Routes>
+        <Route path="/" element={<AnecdoteList anecdotes={anecdotes} />} />
+        <Route path="/create" element={<CreateNew addNew={addNew} />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+};
+
+export default App;
